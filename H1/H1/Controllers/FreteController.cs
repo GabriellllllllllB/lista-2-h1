5bae6b75693181c438b7b78d36115c204b474c5f
GabriellllllllllB <164575563+GabriellllllllllB@users.noreply.github.com>
2025-08-24 23:57:31 -0300

    Add files via upload

---
+++ H1/H1/Controllers/FreteController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MinhaApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FreteController : ControllerBase
+    {
+        public class Produto
+        {
+            public string Nome { get; set; }
+            public float Peso { get; set; }
+            public float Altura { get; set; }
+            public float Largura { get; set; }
+            public float Comprimento { get; set; }
+            public string UF { get; set; }
+        }
+
+        [HttpPost("calcular frete")]
+        public IActionResult CalcularFrete([FromBody] Produto produto)
+        {
+            if (produto.Altura <= 0 || produto.Largura <= 0 || produto.Comprimento <= 0)
+                return BadRequest("Erro, os valores não podem ser zero");
+
+            double volume = produto.Altura * produto.Largura * produto.Comprimento;
+
+            double txPCm3 = 0.01;
+
+            double txestado = produto.UF switch
+            {
+                "SP" => 50.0,
+                "RJ" => 60.0,
+                "MG" => 55.0,
+                _ => 70.0
+            };
+            double valorf = (volume * txPCm3) + txestado;
+
+            return Ok(new
+            {
+                produto.Nome,
+                Volume = volume,
+                UF = produto.UF,
+                ValorFrete = valorf
+            });
+        }
+    }
+}
---
+++ H1/H1/Controllers/PEssoaController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MinhaApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PessoaController : ControllerBase
+    {
+        public class Pessoa
+        {
+            public string Nome { get; set; }
+            public double Peso { get; set; }
+            public double Altura { get; set; }
+        }
+        [HttpPost("imc")]
+        public IActionResult CalcularIMC([FromBody] Pessoa pessoa)
+        {
+            if (pessoa.Altura <= 0)
+                return BadRequest("Erro, altura não pode ser 0");
+
+            double imc = pessoa.Peso / (pessoa.Altura * pessoa.Altura);
+
+            return Ok(new
+            {
+                pessoa.Nome,
+                IMC = imc
+            });
+        }
+        [HttpPost("consulta tabela imc")]
+        public IActionResult ConsultaTabelaIMC([FromBody] Pessoa pessoa)
+        {
+            if (pessoa.Altura <= 0)
+                return BadRequest("Erro, altura não pode ser 0");
+
+            double imc = pessoa.Peso / (pessoa.Altura * pessoa.Altura);
+
+            string resultado = imc switch
+            {
+                < 18.5 => "Abaixo do peso",
+                >= 18.5 and < 24.9 => "Peso normal",
+                >= 25 and < 29.9 => "Sobrepeso",
+                >= 30 and < 34.9 => "Obesidade grau 1",
+                >= 35 and < 39.9 => "Obesidade grau 2",
+                _ => "Obesidade grau 3"
+            };
+
+            return Ok(new
+            {
+                pessoa.Nome,
+                IMC = imc,
+                Resultado = resultado
+            });
+        }
+    }
+}
